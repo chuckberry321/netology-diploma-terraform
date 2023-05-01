8b86bfd0daf7f974a7cdc9370f5a95804f1fdd5a
chuckberry321 <83531330+chuckberry321@users.noreply.github.com>
2023-05-01 13:31:25 +0300

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,15 +1,19 @@
 variable "yc_folder_id" {
   description = "Yandex Cloud Folder ID"
+  default     = "folder id"
 }
 
 variable "yc_cloud_id" {
   description = "Yandex Cloud ID"
+  default     = "cloud id"
 }
 
 variable "yc_region" {
   description = "region"
+  default     = "region"
 }
 
 variable "yc_token" {
   description = "token"
+  default     = "token"
 }
