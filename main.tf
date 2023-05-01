21faac424031a6d76af1dda94b7e3f5cf3ffbbc3
chuckberry321 <83531330+chuckberry321@users.noreply.github.com>
2023-05-01 15:11:14 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+      version = "0.41.0"
+    }
+  }
+}
+
 provider "yandex" {
   folder_id = var.folder_id
   cloud_id = var.cloud_id
