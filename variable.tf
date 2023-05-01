487cce65bb5e17aedea3ccfbce54962186a15419
chuckberry321 <83531330+chuckberry321@users.noreply.github.com>
2023-05-01 13:29:53 +0300

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "yc_folder_id" {
+  description = "Yandex Cloud Folder ID"
+}
+
+variable "yc_cloud_id" {
+  description = "Yandex Cloud ID"
+}
+
+variable "yc_region" {
+  description = "region"
+}
+
+variable "yc_token" {
+  description = "token"
+}
